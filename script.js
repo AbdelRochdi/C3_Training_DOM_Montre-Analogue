7100c0a840b4a9d2dfcfebeb11f4fbf493612f39
AbdelRochdi <abdelghafour.rochdi96@gmail.com>
2020-04-12 18:10:15 +0100

    live coding

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
-const aiguilleHeures = document.querySelector('#hour');
-const aiguilleMinutes = document.querySelector('#minute');
-const aiguilleSeconds = document.querySelector('#second');
+// Séléctionner les aiguilles de montre
+const AIGUILLEHR = document.querySelector('#hour');
+const AIGUILLEMIN = document.querySelector('#minute');
+const AIGUILLESEC = document.querySelector('#second');
 
-function moveClock() {
-	let now = new Date();
-	console.log(now);
+//Extraire l'heure actuel à l'aide de l'objet Date()
 
-	let seconds = now.getSeconds();
-	let minutes = now.getMinutes();
-	let hours = now.getHours();
+// let actualDate = new Date();
+// console.log(actualDate);
 
-	let angleSeconds = seconds * 6;
-	let angleMinutes = minutes * 6;
-	let angleHours = hours * 30;
+// //Ajouter l'heure , minite , seconde  dans des varaiables
+// let hours = actualDate.getHours();
 
-	console.log(angleSeconds);
+// let minutes = actualDate.getMinutes();
+// let seconds = actualDate.getSeconds();
+// console.log(hours);
+// console.log(minutes);
+// console.log(seconds);
 
-	aiguilleSeconds.style.transform = `rotate(${angleSeconds}deg)`;
-	aiguilleMinutes.style.transform = `rotate(${angleMinutes + angleSeconds/60}deg)`;
-	aiguilleHeures.style.transform = `rotate(${angleHours + angleMinutes/60}deg)`;
-	
+// // Calculer de degré de mouvement de l'aiguille heure, de l'aiguille minute, de l'aiguille seconde
+// // Hint : Tous les aiguilles doivent se déplacer chaque second selon un degré
+
+// let hoursAngle = hours * 30;
+// let minutesAngle = minutes * 6;
+// let secondsAngle = seconds * 6;
+setInterval(demarrerLaMontre, 1000);
+// Déplacer les aiguilles
+function demarrerLaMontre() {
+	let actualDate = new Date();
+	let seconds = actualDate.getSeconds() / 60;
+	let minutes = (actualDate.getMinutes() + seconds) / 60;
+	let hours = (actualDate.getHours() + minutes) / 12;
+
+
+	AIGUILLESEC.style.transform = `rotate(${seconds * 360}deg)`;
+	AIGUILLEMIN.style.transform = `rotate(${minutes * 360}deg)`;
+	AIGUILLEHR.style.transform = `rotate(${hours * 360}deg)`;
 }
+// Exercuter la fonction chaque second
 
-moveClock();
-setInterval(moveClock, 1000);
-
+demarrerLaMontre();
